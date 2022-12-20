e68dae82cae069f9b611bcb5bab1207930056229
TecnoProfesor <65020790+TecnoProfesor@users.noreply.github.com>
2022-12-20 17:44:03 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -18,13 +18,26 @@ basic.showIcon(IconNames.Yes)
 joystickbit.initJoystickBit()
 radio.setGroup(23)
 colour = 0
+// If a tool is used change the value to 1
+// If a tool as a excavator shovel is installed on servo S2, the 'C' and 'F' button will be used to move it.
+// 
+// Otherwise the 'C' and 'F' button will move the camera on servo S1
+let tool = 0
 basic.forever(function () {
     radio.sendValue("X", joystickbit.getRockerValue(joystickbit.rockerType.X))
     radio.sendValue("Y", joystickbit.getRockerValue(joystickbit.rockerType.Y))
     if (joystickbit.getButton(joystickbit.JoystickBitPin.P12)) {
-        radio.sendValue("C", 0)
+        if (tool == 0) {
+            radio.sendValue("C", 0)
+        } else {
+            radio.sendValue("C", 1)
+        }
     }
     if (joystickbit.getButton(joystickbit.JoystickBitPin.P15)) {
-        radio.sendValue("F", 0)
+        if (tool == 0) {
+            radio.sendValue("F", 0)
+        } else {
+            radio.sendValue("F", 1)
+        }
     }
 })
